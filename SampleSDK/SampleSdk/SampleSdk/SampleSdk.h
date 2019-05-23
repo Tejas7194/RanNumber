560f162507b4733006ea22df8d0f0276c98d3e3a
Tejas <todkartejas20@gmail.com>
2019-05-23 13:16:34 +0530

    Header file added

---
+++ SampleSDK/SampleSdk/SampleSdk/SampleSdk.h
@@ -0,0 +1,19 @@
+//
+//  SampleSdk.h
+//  SampleSdk
+//
+//  Created by tejas on 22/05/19.
+//  Copyright © 2019 LNT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleSdk.
+FOUNDATION_EXPORT double SampleSdkVersionNumber;
+
+//! Project version string for SampleSdk.
+FOUNDATION_EXPORT const unsigned char SampleSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleSdk/PublicHeader.h>
+
+
